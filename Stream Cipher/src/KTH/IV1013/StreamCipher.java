bd643f45cc86d353fe502357001e6bb72d36a5a8
Lucas Larsson <lucas.a.larsson@gmail.com>
2022-03-26 14:44:43 +0100

    First draft of the algorithm

---
+++ Stream Cipher/src/KTH/IV1013/StreamCipher.java
@@ -0,0 +1,37 @@
+package KTH.IV1013;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+
+public class StreamCipher {
+
+    public static void main(String[] args) {
+
+        Integer key = Integer.valueOf(args[0]);
+        File inputFile = new File(args[1]);
+        File outputFile = new File(args[2]);
+        try (
+               // BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+                // BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+                InputStream in = new BufferedInputStream(new FileInputStream(inputFile));
+                OutputStream out = new BufferedOutputStream(new FileOutputStream(outputFile))
+            )
+        {
+            System.out.println(key);
+            String st;
+           // while ((st = in..readLine()) != null) {
+              //  System.out.println(st);
+                 RC4 r = new RC4(ByteBuffer.allocate(4).putInt(key).array());
+                 byte[] y;
+                y = r.encrypt(in.readAllBytes());
+                out.write(y);
+            //}
+        } catch (IOException e) {
+            System.out.println("Some I/O Error" + e.getMessage());
+            System.exit(1);
+        }
+        // write your code here
+        System.out.println("wazzup22");
+    }
+
+}
