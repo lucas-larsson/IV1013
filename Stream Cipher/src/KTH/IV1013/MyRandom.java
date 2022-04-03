49d4d8cf8802bfc770a4074be68e42cf9492e894
Lucas Larsson <lucas.a.larsson@gmail.com>
2022-04-03 22:19:09 +0200

    Refactored

--- Stream Cipher/src/KTH/IV1013/MyRandom.java
+++ Stream Cipher/src/KTH/IV1013/MyRandom.java
@@ -11,54 +11,48 @@ public class MyRandom extends Random {
 
     long seed;
     byte[] byteKey;
-    int[] sVector = new int[256];
+    int[] s = new int[256];
     int a, b;
 
     public MyRandom(long seed) {
-        setSeed(seed);
-        RC4setup();
+        RC4(); setSeed(seed);
     }
 
-    private void RC4setup() {
-
-        //Key to byte array
+    private void RC4() {
+        // inserts the Key to a byte array s
         byteKey = (seed + "").getBytes();
-
-        //Fill sVector
         for (int i = 0; i < 256; i++) {
-            sVector[i] = i;
+            s[i] = i;
         }
-
-        //Randomize sVector
+        // Randomizes s
         int j = 0;
-        int tmp;
         for (int i = 0; i < 256; i++) {
-            j = (j + sVector[i] + byteKey[i % byteKey.length]) % 256;
-            //Swap
-            tmp = sVector[i];
-            sVector[i] = sVector[j];
-            sVector[j] = tmp;
+            j = (j + s[i] + byteKey[i % byteKey.length]) % 256;
+            swap( i, j);
         }
     }
 
+    // next() method that overrides the superclasses
     @Override
     protected int next(int bits) {
-
         //Increment and randomize
         a = (a + 1) % 256;
-        b = (b + sVector[a]) % 256;
-
-        //Swap
-        int swapper = sVector[a];
-        sVector[a] = sVector[b];
-        sVector[b] = swapper;
-
-        return sVector[(sVector[a] + sVector[b]) % 256];
+        b = (b + s[a]) % 256;
+        swap( b, a );
+        return s[(s[a] + s[b]) % 256];
     }
 
+    // setSeed() method that overrides the superclasses
     @Override
     public synchronized void setSeed(long seed) {
         this.seed = seed;
+    }
+
+    // swap method
+    public void swap(int i, int j){
+        int temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
     }
 }
 
--- Stream Cipher/src/KTH/IV1013/StreamCipher.java
+++ Stream Cipher/src/KTH/IV1013/StreamCipher.java
@@ -4,36 +4,68 @@ import java.io.*;
 
 public class StreamCipher {
 
+//        private static final int BUFFER_SIZE = 8;
+
     public static void main(String[] args) {
-        if (args.length > 3 ) {
-            System.out.println("Too many arguments: " + args.length );
+
+
+        long Key = 0;
+        File inputFile;
+        File outputFile;
+
+        // error handling
+        if (args.length > 3) {
+            System.out.println("Too many arguments: " + args.length);
             System.out.println("Usage: StreamCipher <key> <inputFile> <outputFile>");
             System.exit(-1);
-        }else if(args.length < 3){
-            System.out.println("Too few arguments: " + args.length );
+        } else if (args.length < 3) {
+            System.out.println("Too few arguments: " + args.length);
             System.out.println("Usage: StreamCipher <key> <inputFile> <outputFile>");
             System.exit(-2);
         }
 
-        long Key = Long.parseLong(args[0]);
-        File inputFile = new File(args[1]);
-        File outputFile = new File(args[2]);
+        try {
+            Key = Long.parseLong(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("[" + args[0] + "] is not a Valid Key");
+        }
+
+        inputFile = new File(args[1]);
+        outputFile = new File(args[2]);
 
         MyRandom generator = new MyRandom(Key);
 
+        // I/O
         try (
-                InputStream in = new BufferedInputStream(new FileInputStream(inputFile));
-                OutputStream out = new BufferedOutputStream(new FileOutputStream(outputFile))
-            ){
+                InputStream in = new FileInputStream(inputFile);
+                OutputStream out = new FileOutputStream(outputFile)
+        ) {
+//            byte[] buffer = new byte[BUFFER_SIZE];
+//            int bytesRead = -1;
+//
+//            while ((bytesRead = in.read(buffer)) != -1) {
+//                if (buffer == 48)  bit = 0; else {bit = 1;}
+//                int outputByte = bytesRead ^ generator.next(8);
+////                out.write(buffer, 0, bytesRead);
+//                out.write(outputByte);
+//                System.out.println(Arrays.toString(buffer));
+//                System.out.println(generator.next(8));
+//                System.out.println(outputByte);
+//            }
+
+
             int inputByte = in.read();
             while (inputByte != -1) {
+//                System.out.println(inputByte);
+                // if (inputByte == 48)  bit = 0; else {bit = 1;}
+                // System.out.println(bit);
                 int outputByte = inputByte ^ generator.next(8);
                 out.write(outputByte);
                 inputByte = in.read();
             }
         } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("Some I/O Error" + e.getMessage());
+            e.printStackTrace();        // tracers the error origin
+            System.out.println("Error Message :" + e.getMessage());
             System.exit(1);
         }
     }
